087a9c8fcb2e5003730df0cf27a50baa67c4083e
Elliot J. Reed <elliot.reed@bunches.co.uk>
2019-04-11 19:38:22 +0100

    Adds mutation testing

--- src/ElliotJReed/DisposableEmail/Email.php
+++ src/ElliotJReed/DisposableEmail/Email.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace ElliotJReed\DisposableEmail;
+namespace ElliotJReed\DisposableEmail\Static;
 
 use ElliotJReed\DisposableEmail\Exceptions\InvalidEmailException;
 
