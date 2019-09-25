08966095528b077dc8d0a8d3131bb1da2e59679e
Elliot J. Reed <elliot.reed@bunches.co.uk>
2019-09-25 16:41:20 +0100

    Makes exception class final

--- src/ElliotJReed/DisposableEmail/Exceptions/InvalidEmailException.php
+++ src/ElliotJReed/DisposableEmail/Exceptions/InvalidEmailException.php
@@ -5,7 +5,7 @@ namespace ElliotJReed\DisposableEmail\Exceptions;
 
 use Exception;
 
-class InvalidEmailException extends Exception
+final class InvalidEmailException extends Exception
 {
     protected $message = 'The email address specified is invalid according to RFC 822.';
 }
